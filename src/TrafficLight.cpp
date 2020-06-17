51e5b33112119b43bb35b68616fed164d37529a6
Dana Robinson <derobins@hdfgroup.org>
2020-06-17 02:57:32 -0700

    refactor: Add receive() implementation to the message queue
    Task FP.5a

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -9,7 +9,21 @@ T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // The received object should then be returned by the receive function.
+
+    // Acquire the mutex (will be released when it goes out of scope)
+    std::unique_lock<std::mutex> lock(_mutex);
+
+    // Wait on the condition variable until the message queue is not empty
+    _condition.wait(lock, [this] { return !_queue.empty(); });
+
+    // Pop the message off of the queue
+    T msg = std::move(_queue.front());
+
+    // And delete it, because C++'s back/front peek instead of pop.
+    _queue.pop_front();
+
+    return msg;
 }
 
 template <typename T>
@@ -18,7 +32,7 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 
-    // Acquire the mutex (will be released when we go out of scope)
+    // Acquire the mutex (will be released when it goes out of scope)
     std::lock_guard<std::mutex> lock(_mutex);
 
     // Push the message into the queue and notify waiters
